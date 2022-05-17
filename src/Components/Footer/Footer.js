c4f73184b8263a620d4068362c2d6cab1915a6f9
jchu15 <79078667+jchu15@users.noreply.github.com>
2022-05-17 22:29:16 +0800

    Portfolio Navigation Bar

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../Footer/FooterStyle.css'
+//import { FaFacebook, FaLinkedin, FaGithub } from 'react-icons/fa'
+export const Footer = () => {
+  return (
+    <h1 className="text-3xl font-bold underline text-center">
+    Hello world!
+  </h1>
+  )
+}
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react'
+import { Link, animateScroll as scroll, } from 'react-scroll'
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
+export const Navbar = () => {
+
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
+
+    const handleClose =()=> setNav(!nav)
+  return (
+    <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
+    <div className='px-2 flex justify-between items-center w-full h-full'>
+      <div className='flex items-center'>
+        <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>JSON C.</h1>
+        <ul className='hidden md:flex'>
+        <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
+        <li><Link to="projects" smooth={true} offset={-200} duration={500}>Projects</Link></li>
+        </ul>
+      </div>
+      
+      <div className='md:hidden mr-4' onClick={handleClick}>
+          {!nav ? <MenuIcon className='w-5' /> : <XIcon className='w-5' />}
+        
+      </div>
+    </div>
+
+    <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-8'}>
+        <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="home" smooth={true} duration={500}>Home</Link></li>
+        <li className='border-b-2 border-zinc-300 w-full'><Link onClick={handleClose} to="projects" smooth={true} offset={-200} duration={500}>Projects</Link></li>
+    </ul>
+  </div>
+  )
+}
