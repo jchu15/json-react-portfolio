130db78c14341d5a9813a1178b03efd2b91625e9
jchu15 <79078667+jchu15@users.noreply.github.com>
2022-05-18 15:53:54 +0800

    Portfolio Service Board

---
+++ src/Components/Contents/Services.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import '../Contents/ServiceStyle.css'
+import UI from '../../Assets/ui.png'
+import Web from '../../Assets/web.png'
+import Mobile from '../../Assets/mobile.png'
+const services = () => {
+  return (
+    <div className='services-box'>
+        <div className='container'>
+            <div className='service-item'>
+                <div className='service-item-icon'>
+                    <img className='service-icon' src={UI} alt='ui-ux'/>
+                </div>
+                <div className='service-item-label'>
+                    <p>UI / UX Design</p>
+                </div>
+            </div>
+
+            <div className='service-item'>
+                <div className='service-item-icon'>
+                <img className='service-icon' src={Web} alt='web development'/>
+                </div>
+                <div className='service-item-label'>
+                    <p>Web Development</p>
+                </div>
+            </div>
+
+            <div className='service-item'>
+                <div className='service-item-icon'>
+                <img className='service-icon' src={Mobile} alt='mobile development'/>
+                </div>
+                <div className='service-item-label'>
+                    <p>Mobile Development</p>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default services
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,14 @@
 import React from 'react'
 // import Navbar from '../Components/Navbar/Navbar'
 import Carousel from '../Components/Contents/Carousel'
-import Footer from '../Components/Footer/Footer'
+import Services from '../Components/Contents/Services'
+//import Footer from '../Components/Footer/Footer'
 export const Home = () => {
   return (
     <>
     {/* // <Navbar /> */}
     <Carousel />
-    <Footer />
-    
+    <Services />
     </>
   )
 }
